f1afbd93f92e8cbf100eaa025a8a3c25af0b31b5
Tiago Silva <tianacleto@hotmail.com>
2021-09-23 11:19:00 -0300

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Tiago Silva on 23/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
